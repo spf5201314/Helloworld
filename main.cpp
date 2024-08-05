526bdbe9ec347b1b09dead1bf3cb01c2bea22c22
ubutun-pengfei <2580870834@qq.com>
2024-08-05 17:12:01 +0800

    创建了初始的main.cpp文件,写了初始的代码

---
+++ main.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+void sort(int arr[],int size)
+{
+
+}
+
+int main()
+{
+    int arr[] = {12,15,84,21};
+    int size = sizeof(arr)/sizeof(arr[0]);
+    sort(arr,size);
+    return 0;
+}
