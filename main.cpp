70ddebfd8dd7864e1ca75d4559360cd78872bd1d
ubutun-pengfei <2580870834@qq.com>
2024-08-05 18:08:43 +0800

    添加了冒泡排序

--- main.cpp
+++ main.cpp
@@ -2,7 +2,19 @@
 using namespace std;
 void sort(int arr[],int size)
 {
-
+    int temp =0 ;
+    for(int i=0;i<size-1;++i)
+    {
+        for(int j=0;j<size-1-i;++j)
+        {
+            if(arr[j]>arr[j+1])
+            {
+                temp = arr[j];
+                arr[j] = arr[j+1];
+                arr[j+1] = temp;
+            }
+        }
+    }
 }
 
 int main()
